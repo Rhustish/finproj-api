2d46bc075b67b8e3971abf093457950bf59c540d
Bhavy Bhatnagar <bbhatnagar27@gmail.com>
2024-08-27 10:28:47 +0530

    auth ok <3

--- src/Controllers/authControllers.js
+++ src/Controllers/authControllers.js
@@ -46,16 +46,25 @@ export const loginController = async (req,res)=>{
     if(!findUser){
         return res.status(404).send("User Not Found")
     }
-
-    const passwordHashMatch = await bcrypt.compare(password,findUser.password);
+    console.log(findUser);
+    const passwordHashMatch = await bcrypt.compare(password,findUser.passwordHash);
     if(!passwordHashMatch){
-        return res.status(401).send("passwords mismatch");
+        return res.status(401).send("Invalid user id or password");
     } 
 
     const token = tokenize(findUser._id);
     req.session.token = token;
+    services.redisClient.set(`sess:${req.sessionID}`,24*60*60,token);
+    res.cookie('jwt',token,{
+        httpOnly:true,
+        secure:false, //also set to treu after ssl
+        maxAge: 24 * 60 * 60 * 1000 * 10 * 365
+    })
 
-    res.status(200).send(token);
+    res.status(200).send({
+        auth:"ok",
+        tokenStatus:'jwt-cookie'
+    });
 }
 
 export const signupController = async (req,res)=>{
@@ -64,6 +73,12 @@ export const signupController = async (req,res)=>{
         return res.status(400).send("Missing Creds");
     }
     const [email , password] = credentials;
+
+    const userExists = await UserCollection.findOne({email});
+    if(userExists){
+        return res.status(400).send("User Exists, How about a login?");
+    }
+
     const passwordHash = await bcrypt.hash(password,parseInt(envVars.bsalt));
     const userInfo = req.body;
     const userObject = new UserCollection({
@@ -75,7 +90,18 @@ export const signupController = async (req,res)=>{
     await userObject.save()
     .then(user=>{
         const token = tokenize(user._id);
-        res.status(200).send(token);
+        req.session.token = token;
+        services.redisClient.set(`sess:${req.sessionID}`,24*60*60,token);
+        res.cookie('jwt',token,{
+            httpOnly:true,
+            secure:false, //also set to treu after ssl
+            maxAge: 24 * 60 * 60 * 1000 * 10 * 365
+        })
+
+        res.status(200).send({
+            auth:'ok',
+            tokenStatus:'jwt-cookie'
+        })
     })
     .catch(err=>{
         res.status(400).send(err.message);
